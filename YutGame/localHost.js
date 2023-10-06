6c8058606e5aae0c2c78c57492c1c106301e8024
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-07 00:49:53 +0900

    writeHead() 메서드로 응답 헤더 작성

--- YutGame/localHost.js
+++ YutGame/localHost.js
@@ -3,4 +3,5 @@ const fs = require("fs");
 http.createServer(function (request, response) {
   console.log(request.method);
   console.log(response.url);
+  response.writeHead(200, { "Content-Type": "text/html" });
 });
