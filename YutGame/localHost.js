1caf00ef2cbe020d78c682bbda33cbdc228b3302
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-07 00:59:09 +0900

    yutGame.html로 확장자를 변경하고 읽을 수 있도록 경로 작성

--- YutGame/localHost.js
+++ YutGame/localHost.js
@@ -4,7 +4,5 @@ http.createServer(function (request, response) {
   console.log(request.method);
   console.log(response.url);
   response.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile(, function (){
-    
-  })
+  fs.readFile("./yutGame.html", function (err, data) {});
 });
