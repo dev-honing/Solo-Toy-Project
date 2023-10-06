842fa0c9d37b837fbed90985d589e763c3b3403f
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-07 00:56:54 +0900

    readFile() 메서드 안에 함수 생성

--- YutGame/localHost.js
+++ YutGame/localHost.js
@@ -4,4 +4,7 @@ http.createServer(function (request, response) {
   console.log(request.method);
   console.log(response.url);
   response.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile(, function (){
+    
+  })
 });
