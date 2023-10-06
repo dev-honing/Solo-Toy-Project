88d4b480d2dac79ed14d88b7939f069ecfa63afe
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-07 01:04:46 +0900

    yutGame.html의 제목을 설정하고 포트번호를 8000으로 설정. localHost.js를 실행하고 http://localhost:8000으로 진입해 작동되는지 확인

--- YutGame/localHost.js
+++ YutGame/localHost.js
@@ -1,14 +1,16 @@
 const http = require("http");
 const fs = require("fs");
-http.createServer(function (request, response) {
-  console.log(request.method);
-  console.log(response.url);
-  response.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile("./yutGame.html", function (err, data) {
-    if (err) {
-      console.error("파일을 읽지 못했습니다.");
-    } else {
-      response.end(data);
-    }
-  });
-});
+http
+  .createServer(function (request, response) {
+    console.log(request.method);
+    console.log(response.url);
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile("./yutGame.html", function (err, data) {
+      if (err) {
+        console.error("파일을 읽지 못했습니다.");
+      } else {
+        response.end(data);
+      }
+    });
+  })
+  .listen(8000);
