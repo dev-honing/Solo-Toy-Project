cd6c5fbb561b9bf1523bb6ce6cf9bca9bd945a7a
Tech1997 <dev.honing@gmail.com>
2023-10-11 16:49:01 +0900

    콘솔 출력 문제: 1~10구를 한 번에 던짐

--- HomeRun-Derby/app.js
+++ HomeRun-Derby/app.js
@@ -1,9 +1,12 @@
 const throwBall = setInterval(callback, 2000); // 2초마다 투구
 function callback(){
-  console.log(`제${console.count()}구`)
-  console.log("투수 와인드업");
-  setTimeout(function(){
-    clearInterval(throwBall);
-  }, 30000) // 30초 뒤 종료
+  for(i=1; i<=10; i++){
+    console.log(`제${[i]}구 투수 와인드업`)
+    
+    setTimeout(function(){
+      clearInterval(throwBall);
+    }, 30000) // 30초 뒤 종료
+    }
   }
-  // 콘솔 출력 원하는 방향 : "제n구" + "투수 와인드업"
+  
+  // 1~10구를 한 번에 던져버리는 콘솔 출력 문제 발생
