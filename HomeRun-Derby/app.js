fdabcceb1be63541ba9c559cc9959aba39655885
Tech1997 <dev.honing@gmail.com>
2023-10-11 17:46:22 +0900

    문제 해결! 제어를 위한 핸들러를 초기화해서 반복문 대신 사용

--- HomeRun-Derby/app.js
+++ HomeRun-Derby/app.js
@@ -1,18 +1,11 @@
-// const throwBall = setInterval(callback, 2000); // 2초마다 투구
-// function callback(){
-//   for(i=1; i<=15; i++){
-//     console.log(`제${[i]}구 투수 와인드업`)
-//   }
-// }
+const throwBall = setInterval(callback, 2000); // 2초마다 투구
 
-// setTimeout(function(){
-//   clearInterval(throwBall);
-// }, 10000) // 30초 뒤 종료
-
-
-const interval = setInterval(callback, 2000);
+// 제어를 위한 초기화
+count = 0; 
 function callback(){
-  console.log("hello");
-}
-// 2초마다 콘솔찍는 것은 잘되고 있다.
-// 앞에 연번을 붙일 수 있는지 확인
+  count++;
+  console.log(`제${[count]}구, 투수 와인드업`);
+  }
+setTimeout(function(){
+  clearInterval(throwBall);
+}, 30000) // 30초 뒤 종료
