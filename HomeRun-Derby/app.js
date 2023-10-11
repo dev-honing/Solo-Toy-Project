4375b8a2932770925eaa258df1c286929fe13462
Tech1997 <dev.honing@gmail.com>
2023-10-11 15:27:52 +0900

    setTimeout() 함수에 clearInterval() 함수를 넣어 10초 뒤, 중단하는 로직 작성

--- HomeRun-Derby/app.js
+++ HomeRun-Derby/app.js
@@ -1,8 +1,17 @@
 // 2초마다 투구
-const throwBall = setInterval(callback, 2000);
-function callback(){
-  for(i=1; i<16; i++){
-    console.log(`제${[i]}구 투구`);
-  }
+// const throwBall = setInterval(callback, 2000);
+// function callback(){
+//   for(i=1; i<16; i++){
+//     console.log(`제${[i]}구 투구`);
+//   }
   
-}
+// }
+
+
+const interval = setInterval(callback, 100);
+function callback(){
+  console.log('a');
+  setTimeout(function(){
+    clearInterval(interval);
+  }, 10000)
+  }
