867c0cdb5c92cfe3f2c98efa75912431f30da6c9
Tech1997 <dev.honing@gmail.com>
2023-10-11 15:31:50 +0900

    콘솔이 원하는 대로 출력되지 않음을 확인

--- HomeRun-Derby/app.js
+++ HomeRun-Derby/app.js
@@ -1,17 +1,36 @@
-// 2초마다 투구
-// const throwBall = setInterval(callback, 2000);
-// function callback(){
-//   for(i=1; i<16; i++){
-//     console.log(`제${[i]}구 투구`);
-//   }
-  
-// }
-
-
-const interval = setInterval(callback, 100);
+const throwBall = setInterval(callback, 2000); // 2초마다 투구
 function callback(){
-  console.log('a');
+  console.log(`제${console.count()}구`)
+  console.log("투수 와인드업");
   setTimeout(function(){
-    clearInterval(interval);
-  }, 10000)
+    clearInterval(throwBall);
+  }, 30000) // 30초 뒤 종료
   }
+  // 콘솔 출력 확인
+  2
+default: 2
+app.js:3
+제undefined구
+app.js:3
+투수 와인드업
+app.js:4
+2
+default: 3
+app.js:3
+제undefined구
+app.js:3
+투수 와인드업
+app.js:4
+2
+default: 4
+app.js:3
+제undefined구
+app.js:3
+투수 와인드업
+app.js:4
+2
+default: 5
+app.js:3
+제undefined구
+app.js:3
+투수 와인드업
