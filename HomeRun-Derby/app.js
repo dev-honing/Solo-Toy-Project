017aceac72a383528e15b3975e440216b9cfa61d
Tech1997 <dev.honing@gmail.com>
2023-10-11 16:58:23 +0900

    문제 해결하려면 콘솔에 연번을 추가해 출력할 수 있는지 확인

--- HomeRun-Derby/app.js
+++ HomeRun-Derby/app.js
@@ -1,12 +1,18 @@
-const throwBall = setInterval(callback, 2000); // 2초마다 투구
+// const throwBall = setInterval(callback, 2000); // 2초마다 투구
+// function callback(){
+//   for(i=1; i<=15; i++){
+//     console.log(`제${[i]}구 투수 와인드업`)
+//   }
+// }
+
+// setTimeout(function(){
+//   clearInterval(throwBall);
+// }, 10000) // 30초 뒤 종료
+
+
+const interval = setInterval(callback, 2000);
 function callback(){
-  for(i=1; i<=10; i++){
-    console.log(`제${[i]}구 투수 와인드업`)
-    
-    setTimeout(function(){
-      clearInterval(throwBall);
-    }, 30000) // 30초 뒤 종료
-    }
-  }
-  
-  // 1~10구를 한 번에 던져버리는 콘솔 출력 문제 발생
+  console.log("hello");
+}
+// 2초마다 콘솔찍는 것은 잘되고 있다.
+// 앞에 연번을 붙일 수 있는지 확인
